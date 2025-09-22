fc4b14d2fd9647b10736bd68675285441f335a42
Rahidul Islam <rahiseli@outlook.com>
2025-09-22 11:05:35 +0600

    user and user profile table are created

---
+++ users/managers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import BaseUserManager
+import importlib
+class UserManager(BaseUserManager):
+    def create_user(self,email,password, **extra_fields):
+        """Creates and saves a User with the given email and password"""
+        if not email:
+            raise ValueError("User must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_customer(self,email,password,**extra_fields):
+        """Create a patient with email and password"""
+        customer_role = importlib.import_module('user_account.models').User.Role.CUSTOMER
+        extra_fields.setdefault('role',customer_role)
+        user = self.create_user(email,password,**extra_fields)
+        return user
+
+    def create_superuser(self,email,password,**extra_fields):
+        """"Create a superuser with email and password"""
+        superadmin_role = importlib.import_module('user_account.models').User.Role.ADMIN
+        extra_fields.setdefault('role',superadmin_role)
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        user = self.create_user(email,password,**extra_fields)
+        return user
--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ecommerce_api.base_model import TimeStamp
+from .managers import UserManager
 
 # Create your models here.
+
+# Custom User Model
+class User(AbstractUser):
+    class Role(models.IntegerChoices):
+        ADMIN = 1, "Admin"
+        CUSTOMER = 2, "Customer"
+
+    email = models.EmailField(unique=True)
+    role = models.PositiveSmallIntegerField(choices=Role.choices, default=Role.CUSTOMER)
+    username = None
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+# Base Profile Model
+class UserProfile(TimeStamp):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='%(class)s_profile')
+    phone = models.CharField(max_length=15,blank=True,null=True)
+    address = models.TextField(blank=True,null=True)
+    profile_picture = models.ImageField(upload_to='profile',blank=True,null=True)
+
+    def __str__(self):
+        return self.user.email
