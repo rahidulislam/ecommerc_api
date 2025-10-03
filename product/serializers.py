bb4ec0cfa4cc10d0703dfed3f0c41682d97b647d
Rahidul Islam <rahiseli@outlook.com>
2025-10-03 12:17:55 +0600

    change children using get_children()

--- product/serializers.py
+++ product/serializers.py
@@ -17,13 +17,30 @@ class CategorySerializerBase(serializers.ModelSerializer):
         }
 
 class CategorySerializerList(CategorySerializerBase):
+    children = serializers.SerializerMethodField()
+    product_count = serializers.IntegerField(source='products.count', read_only=True)
+    class Meta(CategorySerializerBase.Meta):
+        fields = CategorySerializerBase.Meta.fields + ("children","product_count",)
+
+
+    def get_children(self, obj):
+        children = obj.children.filter(is_active=True)
+        if obj.children.exists():
+            return CategorySerializerBase(children, many=True).data
+        return []
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        if instance.children.exists():
-            data["children"] = CategorySerializerBase(
-                instance.children.all(), many=True
-            ).data
+        if not data["children"]:
+            data.pop("children")
         return data
+
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     if instance.children.exists():
+    #         data["children"] = CategorySerializerBase(
+    #             instance.children.all(), many=True
+    #         ).data
+    #     return data
 
 class CategorySerializerRetrive(CategorySerializerList):
     def to_representation(self, instance):
